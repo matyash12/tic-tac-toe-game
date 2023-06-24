2ccc1c07dab739be5566df1e7a81b67f7f6e3280
matyash12 <93146910+matyash12@users.noreply.github.com>
2023-06-24 21:40:53 +0200

    Create getdata.php

---
+++ getdata.php
@@ -0,0 +1,32 @@
+<?php
+
+// Replace with your actual MySQL database credentials
+$hostname = 'localhost';
+$username = 'root';
+$password = 'password';
+$database = 'main';
+
+
+$mysqli = new mysqli($hostname, $username, $password, $database);
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+$id = $_GET['id'];
+
+ // Select all records from the "game" table
+ $selectQuery = "SELECT * FROM game WHERE id=".$id;
+ $result = $mysqli->query($selectQuery);
+
+ // Check if any records were found
+ if ($result->num_rows == 1) {
+
+     $row = $result->fetch_assoc();
+
+
+     echo $row;
+ } else {
+     echo "Can't find the game!";
+ }
